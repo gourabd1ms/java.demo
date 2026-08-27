635551253f63092d63ac217cb2b19bd96ae30d88
gourabd1ms <gourabd@morganstanley.com>
2026-08-27 17:51:47 -0400

    Add src/main/java/com/example/App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static String greeting() {
+        return "Hello from Java demo!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greeting());
+    }
+}
